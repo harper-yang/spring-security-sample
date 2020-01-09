f0828336e7d0ce17cd52e190f91dea0f625a9b84
yangzhao <15339190665@163.com>
2020-01-09 18:08:07 +0800

    init

---
+++ src/main/java/harper/github/io/HarperYangApplication.java
@@ -0,0 +1,13 @@
+package harper.github.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarperYangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarperYangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/security/sample/HarperYangApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarperYangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
