8d5116410b4e0c342db582b64e22bfdd48b28c07
yangzhao <15339190665@163.com>
2020-04-11 17:46:41 +0800

    fix --> 111

---
+++ spring-security-core/src/main/java/harper/github/io/config/RibbonClientDefaultConfiguration.java
@@ -0,0 +1,8 @@
+package harper.github.io.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RibbonClientDefaultConfiguration {
+
+}
--- spring-security-web/src/main/java/harper/github/io/SpringSecurityWebApplication.java
+++ spring-security-web/src/main/java/harper/github/io/SpringSecurityWebApplication.java
@@ -2,8 +2,12 @@ package harper.github.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringSecurityWebApplication {
 
     public static void main(String[] args) {
