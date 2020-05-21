026c832fe5ca33b386f34095cb9be6ca6f5926d2
yangzhao <15339190665@163.com>
2020-05-21 19:24:36 +0800

    feat --> update 配置文件

--- spring-security-mq/src/main/java/harper/github/io/MqApplication.java
+++ spring-security-mq/src/main/java/harper/github/io/MqApplication.java
@@ -2,14 +2,11 @@ package harper.github.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class SpringSecurityWebApplication {
+public class MqApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityWebApplication.class, args);
+        SpringApplication.run(MqApplication.class, args);
     }
-
 }
---
+++ spring-security-mq/src/main/java/harper/github/io/config/MqConfiguration.java
@@ -0,0 +1,10 @@
+package harper.github.io.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqConfiguration {
+
+
+
+}
